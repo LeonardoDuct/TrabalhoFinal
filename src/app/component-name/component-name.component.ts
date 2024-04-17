0edf758d42a42e9d37134a3c6e3e52835c466dc1
LeonardoDuct <leonardocampos201791@gmail.com>
2024-04-17 11:36:42 -0300

    TrabalhoFinal

---
+++ src/app/component-name/component-name.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'students-details-component-name',
+  standalone: true,
+  imports: [],
+  templateUrl: './component-name.component.html',
+  styleUrl: './component-name.component.scss'
+})
+export class ComponentNameComponent {
+
+}
